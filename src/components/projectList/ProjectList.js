efa2c340d8319923bb2436fb2d0ab03475ca7afa
Jimmy <56059993+jimmylam1@users.noreply.github.com>
2025-02-12 20:40:55 -0800

    reduce delay in /projects animation

--- src/components/projectList/ProjectList.js
+++ src/components/projectList/ProjectList.js
@@ -7,17 +7,18 @@ import { Project } from '../project/Project'
 import Link from 'next/link'
 import { useRef, useState } from 'react'
 import { useViewport } from '@/hooks/useViewport'
-import TypingHeader from '../typingHeader/TypingHeader'
 import useEffectNoInitialMount from '@/hooks/useEffectNoInitialMount'
 
 export default function ProjectList({headerTextArray, onlyHighlight=false}) {
     const highlightedProjectsRef = useRef(null)
-    const { viewed: headerViewed, isVisible: headerIsVisible } = useViewport(highlightedProjectsRef, 0.9, '0px 0px -100px 0px')
+    // const { viewed: headerViewed, isVisible: headerIsVisible } = useViewport(highlightedProjectsRef, 0.9, '0px 0px -100px 0px')
+    const { viewed: headerViewed, isVisible: headerIsVisible } = useViewport(highlightedProjectsRef, 0.3, '0px 0px -300px 0px')
     const [projectViewed, setProjectViewed] = useState(false) // true if >= 1 projects were viewed, false otherwise
     const [canStart, setCanStart] = useState(true)
 
     useEffectNoInitialMount(() => {
-        const delay = onlyHighlight ? 700 : 1300
+        // const delay = onlyHighlight ? 700 : 1300
+        const delay = 0
 
         if (!projectViewed) {
             setCanStart(false)
@@ -28,16 +29,7 @@ export default function ProjectList({headerTextArray, onlyHighlight=false}) {
     }, [headerViewed])
 
     const highlightedProjectsClasses = `${pageStyle.h2} ${pageStyle.highlightedProjects} ${headerViewed ? animationCss.animateFloatUp : animationCss.hidden}`
-    
-    let header = <h2 ref={highlightedProjectsRef} className={highlightedProjectsClasses}>{headerTextArray.join("")}</h2>
-    if (!onlyHighlight) {
-        header = (
-            <div ref={highlightedProjectsRef} style={{'paddingTop': '120px', 'paddingBottom': '50px'}}>
-                <TypingHeader header={'h2'} textInArray={headerTextArray} delayMs={250} delayCharMs={75}/>
-            </div>
-        )
-    }
-
+    const header = <h2 ref={highlightedProjectsRef} className={highlightedProjectsClasses}>{headerTextArray.join("")}</h2>
     const filteredProjectsArray = projectsArray.filter(p => onlyHighlight ? p.isHighlight : true)
 
     return (
